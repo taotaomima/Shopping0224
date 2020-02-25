ecc5e02aaa3281e67deb3305d5db0838ed13d808
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-02-25 15:50:27 +0800

    add shopping-product-front

---
+++ shopping-product-front/src/main/java/com/gtt/shoppingproductfront/ShoppingProductFrontApplication.java
@@ -0,0 +1,13 @@
+package com.gtt.shoppingproductfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingProductFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingProductFrontApplication.class, args);
+    }
+
+}
---
+++ shopping-product-front/src/test/java/com/gtt/shoppingproductfront/ShoppingProductFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtt.shoppingproductfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingProductFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
