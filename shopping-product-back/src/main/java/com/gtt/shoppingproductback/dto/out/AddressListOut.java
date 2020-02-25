a018c20c969b3c5a96c1539599cab0dec958eb1a
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-02-25 15:45:30 +0800

    add address-back apis

---
+++ shopping-product-back/src/main/java/com/gtt/shoppingproductback/dto/out/AddressListOut.java
@@ -0,0 +1,51 @@
+package com.gtt.shoppingproductback.dto.out;
+
+import java.util.List;
+
+public class AddressListOut {
+    private Integer addressId;
+    private Integer customerId;
+    private List<String> content;
+    private String receiverName;
+    private String receiverMobile;
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<String> getContent() {
+        return content;
+    }
+
+    public void setContent(List<String> content) {
+        this.content = content;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getReceiverMobile() {
+        return receiverMobile;
+    }
+
+    public void setReceiverMobile(String receiverMobile) {
+        this.receiverMobile = receiverMobile;
+    }
+}
