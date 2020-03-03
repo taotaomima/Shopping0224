716f6a39f3ed54649af90798e6f5cadb8867d77f
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-03-03 19:48:52 +0800

    add shopping-store-back image

--- shopping-store-back/src/main/java/com/gtt/shoppingstoreback/controller/imageController.java
+++ shopping-store-back/src/main/java/com/gtt/shoppingstoreback/controller/imageController.java
@@ -1,15 +1,40 @@
 package com.gtt.shoppingstoreback.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.gtt.shoppingstoreback.constant.ClientExceptionConstant;
+import com.gtt.shoppingstoreback.exception.ClientException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/image")
-public class imageController {
+@CrossOrigin
+public class ImageController {
 
+    private List<String> imageExts = Arrays.asList("jpg","jpeg","png");
     @PostMapping("/upload")
-    public String upload(@RequestParam MultipartFile images){return null;}
+    public String upload(@RequestParam MultipartFile image) throws IOException, ClientException {
+        String originalFilename = image.getOriginalFilename();
+        String[] splits = originalFilename.split("\\.");
+        String ext = splits[splits.length - 1];
+        ext = ext.toLowerCase();
+        boolean contains = imageExts.contains(ext);
+        if (!contains){
+            throw new ClientException(ClientExceptionConstant.IMAGE_INVALID_ERRCODE, ClientExceptionConstant.IMAGE_INVALID_ERRMSG);
+        }
+        String uuid = UUID.randomUUID().toString();
+        String filename = String.format("%s.%s", uuid, ext);
+        String filepath = String.format("www/image/%s", filename);
+        try(FileOutputStream out = new FileOutputStream(filepath)){
+            byte[] data = image.getBytes();
+            out.write(data);
+        }
+        return filename;
+
+    }
 }
