3693b86b7b9b3e6875f9f6d74ae6847b35e5d14a
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-03-08 15:19:41 +0800

    add store-front customer

---
+++ shopping-store-front/viewmodels/customer-update-profile.js
@@ -0,0 +1,53 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        username: '',
+        realName: '',
+        mobile: '',
+        email: '',
+        mobileVerified: '',
+        emailVerified: ''
+    },
+    mounted(){
+        console.log('view mmounted');
+        this.getCustomer();
+
+    },
+    methods:{
+        handleUpdateClick(){
+            console.log('update click');
+            this.updateProfile();
+        },
+        getCustomer(){
+            axios.get('/customer/getProdile')
+                .then(function (response) {
+                    console.log(response);
+                    var me = response.data;
+                    app.username = me.username;
+                    app.realName = me.realName;
+                    app.mobile = me.mobile;
+                    app.mobileVerified = me.mobileVerified.toString();
+                    app.email = me.email;
+                    app.emailVerified = me.emailVerified.toString();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        updateProfile(){
+            axios.post('/customer/updateProfile', {
+                realName: this.realName,
+                mobile: this.mobile,
+                email: this.email
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('更新成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+
+})
