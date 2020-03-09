60bcae3af8446f5d1300ad463358c66d37f4d1ba
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-03-09 11:03:47 +0800

    add administrator-front product-search 查询功能

--- shopping-administration-front/viewmodels/product-search.js
+++ shopping-administration-front/viewmodels/product-search.js
@@ -1,33 +1,61 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        pageInfo: '',
-        pageNum: 1
+  el: '#app',
+  data: {
+    pageInfo: '',
+    pageNum: 1,
+    productCode: '',
+    productName: '',
+    price: '',
+    stockQuantity: '',
+    selectedStatus: '',
+    statuses: [
+      { value: 0,label: '上架'},
+      { value: 1,label: '下架'},
+      { value: 2,label: '待审核'}
+    ]
+  },
+  mounted() {
+    console.log('view mounted');
+    this.searchProduct();
+  },
+  methods: {
+    handleSearchClick(){
+      console.log('search click');
+      this.pageNum = 1;
+      this.searchProduct();
     },
-    mounted(){
-         console.log('view mounted');
-         this.searchProduct();
+    handleClearClick(){
+      console.log('clear click');
+      this.productCode='',
+      this.productName='',
+      this.price='',
+      this.stockQuantity='',
+      this.selectedStatus=''
     },
-    methods:{
-        handlePageChange(val){
-            console.log('page change',val);
-            this.pageNum= val;
-            this.searchProduct();
-        },
-        searchProduct(){
-            axios.get('/product/search', {
-                params: {
-                  pageNum: this.pageNum
-                }
-              })
-              .then(function (response) {
-                console.log(response);
-                app.pageInfo = response.data;
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-              
+    handlePageChange(val) {
+      console.log('page change', val);
+      this.pageNum = val;
+      this.searchProduct();
+    },
+    searchProduct() {
+      axios.get('/product/search', {
+        params: {
+          pageNum: this.pageNum,
+          productCode: this.productCode,
+          productName: this.productName,
+          price: this.price,
+          stockQuantity: this.stockQuantity,
+          status: this.selectedStatus
         }
+      })
+        .then(function (response) {
+          console.log(response);
+          app.pageInfo = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
     }
+  }
 })
