d020ef07c668c21ce713e984924541de74a5c8e3
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-03-10 15:14:45 +0800

    添加管理端前台  订单条件查询

--- shopping-administration-front/viewmodels/order-search.js
+++ shopping-administration-front/viewmodels/order-search.js
@@ -1,14 +1,48 @@
 var app = new Vue({
     el: '#app',
     data: {
+        orderId:'',
+        customerName:'',
+        totalPrice:'',
+        selectedStatus:'',
+        statuses:[
+            { value: 0, label: '待处理' },
+            { value: 1, label: '处理中' },
+            { value: 2, label: '待发货' },
+            { value: 3, label: '已发货' },
+            { value: 4, label: '待签收' },
+            { value: 5, label: '已签收' },
+            { value: 6, label: '待支付' },
+            { value: 7, label: '已支付' },
+            { value: 8, label: '取消' },
+            { value: 9, label: '拒绝' },
+            { value: 10, label: '完成' },
+            { value: 11, label: '待评价' },
+            { value: 12, label: '已评价' }
+        ],
+        startTime:'',
+        endTime:'',
         pageInfo: '',
-        pageNum: ''
+        pageNum: 1
     },
     mounted(){
         console.log('view mounted');
         this.searchOrder();
     },
     methods:{
+        handleFindClick(){
+            console.log('search click');
+            this.searchOrder();
+        },
+        handleClearClick(){
+            console.log("clear click");
+            this.orderId = '';
+            this.customerName = '';
+            this.totalPrice = '';
+            this.selectedStatus = '';
+            this.startTime = '';
+            this.endTime = '';
+        },
         handlePageChange(val) {
             console.log('page changed', val);
             this.pageNum = val;
@@ -17,7 +51,13 @@ var app = new Vue({
         searchOrder() {
             axios.get('/order/search', {
                 params: {
-                    pageNum: this.pageNum
+                    pageNum: this.pageNum,
+                    orderId: this.orderId,
+                    customerName:this.customerName,
+                    status:this.selectedStatus,
+                    totalPrice:this.totalPrice,
+                    startTime:this.startTime ? this.startTime.getTime():'',
+                    endTime:this.endTime ? this.endTime.getTime():''
                 }
             })
                 .then(function (response) {
