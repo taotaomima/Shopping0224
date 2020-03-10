77395f203bc5af2950bb716798b1c0c8f82a21fc
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-03-10 15:32:58 +0800

    添加管理端前台 退货页条件查询

--- shopping-administration-front/viewmodels/return-search.js
+++ shopping-administration-front/viewmodels/return-search.js
@@ -1,6 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
+        returnId:'',
+        orderId:'',
+        customerName:'',
+        productCode:'',
+        productName:'',
+        selectedStatus:'',
+        statuses:[
+            { value: 0, label: '待处理' },
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '完成' },
+            { value: 4, label: '拒绝' }
+        ],
+        startTime:'',
+        endTime:'',
         pageInfo:'',
         pageNum:1
     },
@@ -9,6 +24,22 @@ var app = new Vue({
         this.getReturnSearch();
     },
     methods:{
+        handleFindClick(){
+            console.log('search click');
+            this.pageNum=1;
+            this.getReturnSearch();
+        },
+        handleClearClick(){
+            console.log('clear click');
+            this.returnId = '';
+            this.orderId = '';
+            this.customerName='';
+            this.productCode='';
+            this.productName='';
+            this.selectedStatus='';
+            this.startTime='';
+            this.endTime='';
+        },
         handlePageChange(val){
             console.log('change page',val);
             this.pageNum=val;
@@ -17,7 +48,15 @@ var app = new Vue({
         getReturnSearch(){
             axios.get('/return/search', {
                 params: {
-                    pageNum: this.pageNum
+                    pageNum: this.pageNum,
+                    returnId:this.returnId,
+                    orderId:this.orderId,
+                    customerName:this.customerName,
+                    productCode:this,productCode,
+                    productName:this.productName,
+                    status:this.selectedStatus,
+                    startTime:this.startTime,
+                    endTime:this.endTime
                 }
             })
                 .then(function (response) {
