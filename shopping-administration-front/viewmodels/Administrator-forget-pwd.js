19149d3f0032751d5a104d186cc4d2c44eaf9104
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-03-19 15:36:19 +0800

    fix page loading

--- shopping-administration-front/viewmodels/Administrator-forget-pwd.js
+++ shopping-administration-front/viewmodels/Administrator-forget-pwd.js
@@ -1,11 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
-        email:''
+        email:'',
+        loading:false,
+        EnableDisable: true,
+        counter:60
     },
     methods:{
         handleFindPwd(){
             console.log('find back pwd click');
+            this.loading = true;
+            this.EnableDisable = false;
+            this.counter = 60;
+             
+            setInterval(function(){
+              console.log("count down");
+              app.counter--;
+              if(app.counter<0){
+                 app.EnableDisable = true;
+              }
+            },1000);
+
             this.getPwdRestCode();
         },
         getPwdRestCode(){
@@ -16,6 +31,7 @@ var app = new Vue({
               })
               .then(function (response) {
                 console.log(response);
+                app.loading = false;
                 alert('重置码发送邮箱成功');
               })
               .catch(function (error) {
