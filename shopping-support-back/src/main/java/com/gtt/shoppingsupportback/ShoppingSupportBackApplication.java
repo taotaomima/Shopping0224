a097ce9dc3957cc250730ec25c4221d9d8d23d6b
CN-20190929WGQE\Administrator <3376165810@qq.com>
2020-03-20 17:20:14 +0800

    first init

---
+++ shopping-support-back/src/main/java/com/gtt/shoppingsupportback/ShoppingSupportBackApplication.java
@@ -0,0 +1,13 @@
+package com.gtt.shoppingsupportback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingSupportBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingSupportBackApplication.class, args);
+    }
+
+}
---
+++ shopping-support-back/src/test/java/com/gtt/shoppingsupportback/ShoppingSupportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtt.shoppingsupportback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingSupportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
